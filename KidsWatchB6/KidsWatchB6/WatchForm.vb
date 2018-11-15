24c885ba15b1d2ed39f0f9aa059462fab2bff083
Jonathan Sit <jonathan.sit@ryerson.ca>
2018-11-15 17:02:03 -0500

    Added phone panel, need to do work on

--- KidsWatchB6/KidsWatchB6/WatchForm.vb
+++ KidsWatchB6/KidsWatchB6/WatchForm.vb
@@ -2,12 +2,13 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TimeLabel.Text = TimeOfDay.ToString("hh:mm tt")
         DateLabel.Text = TimeOfDay.ToString("D")
-
+        PhoneApp.Show()
         'Make all other panels not visible
         ContactPanel.Visible = False
         PaymentPanel.Visible = False
         SchedulePanel.Visible = False
         GamesPanel.Visible = False
+        MusicPanel.Visible = False
     End Sub
 
     Private Sub RightButton_Click(sender As Object, e As EventArgs) Handles RightButton.Click
