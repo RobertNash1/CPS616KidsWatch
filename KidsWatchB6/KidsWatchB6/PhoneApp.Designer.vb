2664cd28da21cfacfd3f41cb0b5e6ea2407c3b39
Braeden <braeden.lem@gmail.com>
2018-11-19 02:14:49 -0500

    nicer phone home page need images

--- KidsWatchB6/KidsWatchB6/PhoneApp.Designer.vb
+++ KidsWatchB6/KidsWatchB6/PhoneApp.Designer.vb
@@ -33,10 +33,25 @@ Partial Class PhoneApp
         Me.TimeLabel2 = New System.Windows.Forms.Label()
         Me.AppPanel = New System.Windows.Forms.Panel()
         Me.TimeLabel3 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
         Me.ClockPanel.SuspendLayout()
         Me.HomePanel.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.AppPanel.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ClockPanel
@@ -79,6 +94,16 @@ Partial Class PhoneApp
         '
         'HomePanel
         '
+        Me.HomePanel.Controls.Add(Me.PictureBox6)
+        Me.HomePanel.Controls.Add(Me.Label4)
+        Me.HomePanel.Controls.Add(Me.PictureBox4)
+        Me.HomePanel.Controls.Add(Me.Label5)
+        Me.HomePanel.Controls.Add(Me.PictureBox5)
+        Me.HomePanel.Controls.Add(Me.Label6)
+        Me.HomePanel.Controls.Add(Me.Label3)
+        Me.HomePanel.Controls.Add(Me.PictureBox3)
+        Me.HomePanel.Controls.Add(Me.Label2)
+        Me.HomePanel.Controls.Add(Me.PictureBox2)
         Me.HomePanel.Controls.Add(Me.Label1)
         Me.HomePanel.Controls.Add(Me.PictureBox1)
         Me.HomePanel.Controls.Add(Me.TimeLabel2)
@@ -90,7 +115,7 @@ Partial Class PhoneApp
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(3, 129)
+        Me.Label1.Location = New System.Drawing.Point(3, 104)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(85, 13)
         Me.Label1.TabIndex = 2
@@ -99,9 +124,9 @@ Partial Class PhoneApp
         'PictureBox1
         '
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(14, 49)
+        Me.PictureBox1.Location = New System.Drawing.Point(18, 49)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(72, 77)
+        Me.PictureBox1.Size = New System.Drawing.Size(60, 52)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
@@ -134,6 +159,91 @@ Partial Class PhoneApp
         Me.TimeLabel3.TabIndex = 0
         Me.TimeLabel3.Text = "12:59 PM"
         '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(93, 49)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(60, 52)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(98, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Facebook"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(173, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Twitter"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(168, 49)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(60, 52)
+        Me.PictureBox3.TabIndex = 5
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Location = New System.Drawing.Point(93, 132)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(60, 52)
+        Me.PictureBox5.TabIndex = 9
+        Me.PictureBox5.TabStop = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(28, 187)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Phone"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(173, 187)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Browser"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(111, 187)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Text"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(168, 132)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(60, 52)
+        Me.PictureBox4.TabIndex = 11
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Location = New System.Drawing.Point(18, 132)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(60, 52)
+        Me.PictureBox6.TabIndex = 13
+        Me.PictureBox6.TabStop = False
+        '
         'PhoneApp
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -152,6 +262,11 @@ Partial Class PhoneApp
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.AppPanel.ResumeLayout(False)
         Me.AppPanel.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -166,4 +281,14 @@ Partial Class PhoneApp
     Friend WithEvents AppPanel As Panel
     Friend WithEvents TimeLabel3 As Label
     Friend WithEvents HScrollBar1 As HScrollBar
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
